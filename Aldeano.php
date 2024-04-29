15a9a2be5f130a4efa8f9d0abef8015a91946153
Tomas Roma <tomas.roma@davinci.edu.ar>
2024-04-29 16:34:23 -0300

    prueba1

---
+++ Aldeano.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once("arbusto.php");
+
+
+    Class Aldeano{
+        private $velocidadRecoleccion;
+
+         function __construct(){
+            $this->velocidadRecoleccion = 18;
+         }
+
+         public function recolectar(Arbusto $arbusto){
+
+            $cantidadAlimento = $arbusto ->getAlimento();
+
+            $tiempo = ceil($cantidadAlimento/this->$velocidadRecoleccion);
+
+            echo "El aldeano recolecto todos los arbustos en ".$tiempo." minutos";
+         }
+    }
+
+
+
+
+
+?>
