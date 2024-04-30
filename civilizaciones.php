f3c461055776aefca8d3045f52e561153fe6265f
Tomas Roma <tomas.roma@davinci.edu.ar>
2024-04-30 20:45:11 -0300

    prueba 16

--- civilizaciones.php
+++ civilizaciones.php
@@ -11,19 +11,39 @@ require_once("header.php");
     <h1>Civilizaciones</h1>
     <section class="section-alemanes">
         <h2>Alemanes</h2>
+        <img src="img/alemanes.jpg" alt="bandera imperio aleman">
+        <article>
+            <p>Los alemanes son una de las civilizaciones más destacadas en el juego Age of Empires. Su fuerza militar y su enfoque en la tecnología los convierten en una potencia formidable en el campo de batalla.</p>
+        </article>
+
 
     </section>
 
     <section class="section-otomanos">
         <h2>Otomanos</h2>
+        <img src="img/otomanos.jpg" alt="bandera imperio otomano">
+        <article>
+            <p>Los otomanos son una civilización poderosa conocida por su economía robusta y su formidable ejército. Su capacidad para generar recursos rápidamente les permite expandirse rápidamente y dominar en el campo de batalla.</p>
+        </article>
+
     </section>
 
     <section class="section-chinos">
         <h2>Chinos</h2>
+        <img src="img/chinos.jpg" alt="bandera imperio chino">
+        <article>
+            <p>Los chinos son una civilización versátil que se destaca en la tecnología y la defensa. Con una amplia gama de opciones de unidades y mejoras, pueden adaptarse a diferentes estrategias y dominar en cualquier situación.</p>
+        </article>
+
     </section>
 
     <section class="section-franceses">
         <h2>Franceses</h2>
+        <img src="img/franceses.jpg" alt="bandera imperio frances">
+        <article>
+            <p>Los franceses son una civilización conocida por su fuerte enfoque en la caballería y la defensa. Su capacidad para construir rápidamente castillos y fortificaciones les proporciona una sólida base desde la cual pueden lanzar ataques y defenderse eficazmente.</p>
+        </article>
+
     </section>
 
   
