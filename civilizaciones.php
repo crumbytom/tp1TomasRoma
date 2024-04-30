884f89324735eb29da37134bea255c30366d6e57
Tomas Roma <tomas.roma@davinci.edu.ar>
2024-04-30 19:24:51 -0300

    prueba16

--- civilizaciones.php
+++ civilizaciones.php
@@ -7,6 +7,31 @@ require_once("header.php");
 ?>
 
 
+<main class="main-civilizaciones">
+    <h1>Civilizaciones</h1>
+    <section class="section-alemanes">
+        <h2>Alemanes</h2>
+
+    </section>
+
+    <section class="section-otomanos">
+        <h2>Otomanos</h2>
+    </section>
+
+    <section class="section-chinos">
+        <h2>Chinos</h2>
+    </section>
+
+    <section class="section-franceses">
+        <h2>Franceses</h2>
+    </section>
+
+  
+
+
+</main>
+
+
 
 <?php
     include_once("footer.php"); // solo llamo al footer
